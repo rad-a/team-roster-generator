fdcab558166d5caebff62de4e2235dc3caabf360
rad-a <11688089+rad-a@users.noreply.github.com>
2020-07-05 20:53:05 -0500

    Removed notes file

--- notes.js
+++
@@ -1,8 +0,0 @@
-//TODO:
-
-// ****** IF enough time, refactor HTML to use UIkit ******
-//1. Define an employee class for all employees (name, email, id, department) --PARENT CLASS
-   // 1a. Additional data based on role: (Manager: Office number; Intern: school; Engineer: github )
-
-
-
